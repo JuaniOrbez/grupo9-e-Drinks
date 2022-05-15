28fd7a39dfa5021d8262bd4c6c5334bcb90df387
JuaniOrbez <orbezjuanignacio@gmail.com>
2022-05-15 15:49:26 -0300

    partials

--- app.js
+++ app.js
@@ -2,33 +2,44 @@ const express = require ('express');
 
 const app = express();
 
+// const mainRoutes = require ('./routes/mainRoutes');
+
+app.use(express.static('public'));
+
+ app.set('view engine','ejs');
+
+// app.use('/', mainRoutes);
+
 const port = 3000
 
 const path = require ('path')
 
-app.listen(port, ()=>console.log("Servidor corriendo en el puerto 3000"))
+app.listen(port, ()=> {
+    console.log("Servidor corriendo en el puerto 3000");
+})
 
-app.use(express.static('public'))
+
 
 app.get ("/", (req, res) =>{
-    res.sendFile(path.join(__dirname,"/views/home.html"))
+    res.render ('home')
 })
 
 app.get ("/productdetail", (req, res) =>{
-    res.sendFile(path.join(__dirname,"/views/productDetail.html"))
+    res.render ('productDetail')
 })
 
 app.get ("/productcart", (req, res) =>{
-    res.sendFile(path.join(__dirname,"/views/productCart.html"))
+    res.render ('productCart')
 })
 
 app.get ("/register", (req, res) =>{
-    res.sendFile(path.join(__dirname,"/views/register.html"))
+    res.render ('register')
 })
 app.get ("/login", (req, res) =>{
-    res.sendFile(path.join(__dirname,"/views/login.html"))
+    res.render('login')
 })
 
 app.get ("*", (req, res) =>{
     res.status (404).send('Not Found 404')
 })
+
