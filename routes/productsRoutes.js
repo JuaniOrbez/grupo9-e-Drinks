05f5b038ea02a846995d3691463087249b6c0f70
Andres Pando <apando@tsa.local>
2022-06-05 17:31:23 -0300

    actualizo productsCreate y productsController

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -12,7 +12,7 @@ const storage = multer.diskStorage({
     },
 
     filename: function (req, file, cb) {
-      cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+      cb(null, file.fieldname + 'img' + Date.now() + path.extname(file.originalname))
     }
   })
   
