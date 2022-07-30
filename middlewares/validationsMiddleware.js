7caabc7396f126f33e79211f79895d651f8a4571
catadecaboteau <cdecaboteau@gmail.com>
2022-07-30 18:00:19 -0300

    arreglos y ajustes

--- middlewares/validationsMiddleware.js
+++ middlewares/validationsMiddleware.js
@@ -1,30 +1,39 @@
 const path = require('path');
-const { body } = require('express-validator');
+const { check } = require('express-validator');
 
 const validationsUsersRegister = [
+    check("first_name")
+        .notEmpty()
+        .isLength({min:2})
+        .withMessage("Debes ingresar un nombre"),
+    check("last_name")
+        .notEmpty()
+        .isLength({min:2})
+        .withMessage("Debes ingresar un apellido"),
+    check("email")
+        .notEmpty().withMessage("Debes ingresar un email").bail()
+        .isEmail().withMessage("Debes ingresar un formato de email válido"),
+    check("password")
+        .notEmpty()
+        .withMessage("Debes ingresar una contraseña"),
+    check("age")
+        .notEmpty()
+        .withMessage("Debes seleccionar una opción"),
+    check('image')
+        .custom((value, { req }) => {
+        let file = req.file;
+        let acceptedExtensions = ['.jpg', '.png', '.gif'];
     
-    body("first_name").notEmpty().withMessage("Debes ingresar un nombre"),
-    body("last_name").notEmpty().withMessage("Debes ingresar un apellido"),
-    body("email")
-      .notEmpty().withMessage("Debes ingresar un email").bail()
-      .isEmail().withMessage("Debes ingresar un formato de email válido"),
-    body("password").notEmpty().withMessage("Debes ingresar una contraseña"),
-    // body("category_id").notEmpty().withMessage("Debes seleccionar una categoría"),
-    body("age").notEmpty().withMessage("Debes seleccionar una opción"),
-    body('image').custom((value, { req }) => {
-      let file = req.file;
-      let acceptedExtensions = ['.jpg', '.png', '.gif'];
-  
-      if (!file) {
-        throw new Error('Debes subir una imagen');
-      } else {
-        let fileExtension = path.extname(file.originalname);
-        if (!acceptedExtensions.includes(fileExtension)) {
-          throw new Error(`Los formatos de archivos permitidos son ${acceptedExtensions.join(', ')}`);
+        if (!file) {
+          throw new Error('Debes subir una imagen');
+        } else {
+          let fileExtension = path.extname(file.originalname);
+          if (!acceptedExtensions.includes(fileExtension)) {
+            throw new Error(`Los formatos de archivos permitidos son ${acceptedExtensions.join(', ')}`);
+          }
         }
-      }
-      return true;
-    })
+        return true;
+       })
 ];
 
 module.exports = validationsUsersRegister;
