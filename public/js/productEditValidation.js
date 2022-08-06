6388fff0d3a9b14c5897f4b770118b34826424aa
JuaniOrbez <orbezjuanignacio@gmail.com>
2022-08-06 19:48:10 -0300

    validacion y cambios en forms

---
+++ public/js/productEditValidation.js
@@ -0,0 +1,15 @@
+window.onload = function(){
+    const form = document.querySelector(".f-registro")
+    form.addEventListener("submit", (e) =>{
+        let emptyElements = []
+        form.querySelectorAll("input").forEach(input =>{
+            if(input.value ===""){
+                emptyElements.push(input)
+            }
+        })
+        if(emptyElements.length >0){
+            e.preventDefault()
+        }
+    })
+    
+    }
