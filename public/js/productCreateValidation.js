de28acde77b2e89f38ac0e14d79b0acb4214d244
JuaniOrbez <orbezjuanignacio@gmail.com>
2022-08-23 14:36:07 -0300

    validacion creacion producto

--- public/js/productCreateValidation.js
+++ public/js/productCreateValidation.js
@@ -1,15 +1,91 @@
 window.onload = function(){
 const form = document.querySelector(".f-registro")
-form.addEventListener("submit", (e) =>{
-    let emptyElements = []
-    form.querySelectorAll("input").forEach(input =>{
-        if(input.value ===""){
-            emptyElements.push(input)
+const formElements = form.querySelectorAll(".formElement")
+const name = document.querySelector("#name")
+const erroresInput = form.querySelectorAll(".erroresInput")
+const errores = document.querySelector(".errores")
+const erroresName = document.querySelector(".erroresName")
+const erroresDescription = document.querySelector(".erroresDescription")
+const erroresImage = document.querySelector(".erroresImage")
+const imagen = document.querySelector(".imagen")
+console.log(erroresInput)
+
+name.focus()
+    
+    formElements.forEach(formElement => formElement.addEventListener('blur', e=>{
+        
+        
+        const element = e.srcElement
+        const p = document.querySelector("#error" + element.id)
+        
+    if (element.value ==="" && element.name === "image"){
+        p.innerHTML = 'El archivo '+ element.placeholder +' debe ser un archivo válido (JPG, JPEG, PNG, GIF)'
+            element.classList.add("isInvalid")
+            emptyElements.push(formElement)
         }
-    })
-    if(emptyElements.length >0){
-        e.preventDefault()
-    }
-})
+        
+        else if (element.value ==="" ){
+            p.innerHTML = 'El campo '+ element.placeholder +' no puede estar vacio'
+            element.classList.add("isInvalid")
+            emptyElements.push(formElement)
+        }
+        else if((element.name === "name") && element.value.length < 5){
+            p.innerHTML = 'El campo '+ element.placeholder +' debe tener al menos 5 caracteres'
+            element.classList.add("isInvalid")
+            emptyElements.push(formElement)
+        }
+        
+        else if((element.name === "description") && element.value.length < 20){
+            p.innerHTML = 'El campo '+ element.placeholder +' debe tener al menos 20 caracteres'
+            element.classList.add("isInvalid")
+            emptyElements.push(formElement)
+        }
+        else if((element.name === "image")){
+            p.innerHTML = 'El archivo '+ element.placeholder +' debe ser un archivo válido (JPG, JPEG, PNG, GIF)'
+            element.classList.add("isInvalid")
+            emptyElements.push(formElement)
+        }
+        
+        else{
+            element.classList.remove("isInvalid")
+            p.innerHTML = ""
+        }
+        
+    }))
+    
+    form.addEventListener("submit", (e) =>{
+        let errores = []
+
+        
+       
+        formElements.forEach(formElement =>{ 
+            const p = document.querySelector("#error" + formElement.id)
+
+           if(formElement.value ===""){  
+            formElement.classList.add("isInvalid")
+            p.innerHTML = 'El campo ' + formElement.placeholder + ' no puede estar vacio'
+                 errores.push(formElement)
+             }
+            
+         if ((formElement.name ==="name") && formElement.value.length < 5){
+                formElement.classList.add("isInvalid")
+                p.innerHTML= 'El campo '+ formElement.placeholder +' debe tener al menos 5 caracteres'    
+                errores.push(formElement)
+            } 
+
+          if  ((formElement.name ==="description") && formElement.value.length < 20){
+            formElement.classList.add("isInvalid")
+            p.innerHTML= 'El campo '+ formElement.placeholder +' debe tener al menos 20 caracteres'  
+            errores.push(formElement)
+        } 
+        })
+    
+        if(errores.length >0){
+            console.log("no send form")
+            e.preventDefault()
+        }
+        
+
+        })
 
 }
